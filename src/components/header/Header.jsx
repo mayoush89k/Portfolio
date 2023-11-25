8d4909fd53b74502d7c33df5c830eb9d274003db
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2023-11-25 23:04:23 +0200

    adding responsive media styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./header.css";
 import logo from "../../assets/logo.png";
+import { FaList } from "react-icons/fa6";
 
 export default function Header() {
   const nav = [
@@ -20,10 +21,13 @@ export default function Header() {
         {nav.map((link, index) => (
           <ul key={index} >
             <li>
-              <a href={link.href} id="l">{link.name}</a>
+              <a href={link.href}>{link.name}</a>
             </li>
           </ul>
         ))}
+      </div>
+      <div className="hidden">
+        <FaList />
       </div>
     </div>
   );
