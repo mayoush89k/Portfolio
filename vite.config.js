3f02eee916e33d5fae65c5d94033ced98d19e815
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2023-11-25 19:58:55 +0200

    Portfolio

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+//// BEGINNING OF MODIFICATION 
+///  add build property here ->
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+//// END OF MODIFICATION 
+});
